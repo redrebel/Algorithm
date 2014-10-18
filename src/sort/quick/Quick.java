fb8675b247f69c262c66051f3b7b3ebe8a603a6b
redrebel <cjred77@gmail.com>
2014-10-19 00:05:52 +0900

    modify quicksort

--- src/sort/quick/Quick.java
+++ src/sort/quick/Quick.java
@@ -5,13 +5,19 @@ package sort.quick;
  */
 public class Quick {
 
-    private static void qsort(int[] arr, int left, int right){
+    /**
+     * Left Pivot
+     * @param arr
+     * @param left
+     * @param right
+     */
+    private static void qsort_left(int[] arr, int left, int right){
         if(left >= right){
             return;
         }
-        for(int a=left;a<=right;a++)
-            System.out.print(arr[a]+",");
-        System.out.println();
+//        for(int a=left;a<=right;a++)
+//            System.out.print(arr[a]+",");
+//        System.out.println();
         int pivotIndex = left;
         int storeIndex = pivotIndex+1;
 
@@ -29,11 +35,42 @@ public class Quick {
         arr[pivotIndex] = arr[storeIndex-1];
         arr[storeIndex-1] = temp;
 
-        qsort(arr, pivotIndex, storeIndex-2);
-        qsort(arr, storeIndex, right);
+        qsort_left(arr, pivotIndex, storeIndex-2);
+        qsort_left(arr, storeIndex, right);
     }
+
+    private static void qsort_middle(int[] arr, int left, int right){
+        int i = left;
+        int j = right;
+        int pivot = arr[(i+j)/2];
+
+//        for(int a=left;a<=right;a++)
+//            System.out.print(arr[a]+",");
+//        System.out.println();
+
+        while(i<=j){
+            while(arr[i]<pivot) i++;
+            while(arr[j]>pivot) j--;
+
+            if(i <= j){
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                i++;
+                j--;
+            }
+        }
+
+
+        if(left<j) qsort_middle(arr,left,j);
+        if(i<right) qsort_middle(arr, i, right);
+
+    }
+
     public static void sort(int[] arr){
-        qsort(arr,0,arr.length-1);
+
+        qsort_left(arr,0,arr.length-1);
+        qsort_middle(arr, 0, arr.length-1);
     }
 
     public static void main(String[] args) {
