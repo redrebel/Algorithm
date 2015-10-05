0b8a16a06175c3378fa468f0c349fa1d8127f220
Lee junggeun <cjred77@gmail.com>
2015-10-05 17:54:14 +0900

    modify config improve selection.java make test Array set

--- src/sort/selection/Selection.java
+++ src/sort/selection/Selection.java
@@ -9,6 +9,16 @@ package sort.selection;
  */
 public class Selection {
 
+	/**
+	 * print array
+	 * @param arr
+	 */
+	public static void printArray(int arr[]){
+		for(int a:arr)
+			System.out.print(a+",");
+		System.out.println();
+	}
+
 	public static void sort(int[] arr, int arrLen){
 		int actCnt = 0;
 		for(int i = 0; i < arrLen; i++){
@@ -24,18 +34,39 @@ public class Selection {
 			int temp = arr[i];
 			arr[i] = arr[miniIdx];
 			arr[miniIdx] = temp;
+			printArray(arr);
 		}
 		System.out.println(actCnt);
 	}
+
+	public static void sort2(int[] arr, int arrLen){
+		int actCnt = 0;
+		for(int i = 0; i < arrLen; i++){
+			int miniIdx = i;
+			for(int j=i+1;j<arrLen;j++){
+				if(arr[miniIdx] > arr[j]){
+					miniIdx = j;
+				}
+				actCnt++;
+			}
+			int temp = arr[i];
+			arr[i] = arr[miniIdx];
+			arr[miniIdx] = temp;
+		}
+		System.out.println(actCnt);
+	}
+
 	
 	public static void main(String[] args) {
-		int[] arr = { 3, 4, 16, 2 ,8, 32, 88, 1};
-		for(int a:arr)
-			System.out.print(a+",");
-		System.out.println();
-		sort(arr, arr.length);
-		for(int a:arr)
-			System.out.print(a+",");
+		int arr[] = new int[8];
+
+		// random array
+		for(int i=0; i < arr.length; i++){
+			arr[i] = (int)(Math.random()*100) + 1;
+		}
+		printArray(arr);
+		sort2(arr, arr.length);
+		printArray(arr);
 	}
 
 }
