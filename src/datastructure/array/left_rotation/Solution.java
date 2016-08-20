768800ff00c2f6d4caaf8be73cc6d4f956944448
Lee junggeun <cjred77@gmail.com>
2016-08-20 11:27:12 +0900

    merge and improve left rotation

--- src/datastructure/array/left_rotation/Solution.java
+++ src/datastructure/array/left_rotation/Solution.java
@@ -15,22 +15,7 @@ public class Solution {
     System.out.println();
   }
 
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    // the number of integer
-    int n = sc.nextInt();
-    // the number of left rotations
-    int d = sc.nextInt();
-
-    // cut the rotation size
-    d = d%n;
-
-    int[] arr = new int[n];
-    for(int i=0; i<n; i++ ){
-      arr[i] = sc.nextInt();
-    }
-    //printArr(arr);
-
+  public static void leftRotation1(int[] arr, int n, int d){
     int[] otherArr = new int[n];
     for(int i=0; i<n; i++){
       // point that have to left rotation move
@@ -38,5 +23,63 @@ public class Solution {
       otherArr[pos] = arr[i];
     }
     printArr(otherArr);
+  }
+
+  public static void leftRotation2(int[] arr, int n, int d){
+    // cut the rotation size
+    d = d%n;
+
+    int[] otherArr = new int[n];
+
+    System.arraycopy(arr,0,otherArr,n-d,d);
+    System.arraycopy(arr,d,otherArr,0,n-d);
+
+    printArr(otherArr);
+
+  }
+
+  public static void leftRotation3(int[] arr, int n, int d){
+    if(n/2==d && n%2==0){
+      // if shift n/2 and n is even then
+      leftRotation2(arr, n, d);
+      return;
+    }
+    //printArr(arr);
+    int pos = 0;
+    int data = arr[pos];
+    for(int i=0;i<n; i++){
+      // pick
+      pos = (pos+n-d)%n;
+      int tmp = arr[pos];
+      arr[pos] = data;
+      data = tmp;
+      if(pos==0){
+        pos=1;
+        data=arr[pos];
+      }
+      //System.out.println(i + ": " + pos + "," + tmp + "," + arr[pos]);
+    }
+    printArr(arr);
+
+  }
+
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    // the number of integer
+    int n = sc.nextInt();
+    // the number of left rotations
+    int d = sc.nextInt();
+
+    int[] arr = new int[n];
+    for(int i=0; i<n; i++ ){
+      arr[i] = sc.nextInt();
+    }
+    //printArr(arr);
+
+    leftRotation1(arr, n, d);
+    leftRotation2(arr, n, d);
+    leftRotation3(arr, n, d);
+    sc.close();
   }
 }
