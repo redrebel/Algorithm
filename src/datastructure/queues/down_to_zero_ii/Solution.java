6ddc2ca3e82b320d415c49ec4580958f509e069a
Lee junggeun <cjred77@gmail.com>
2016-11-04 11:43:35 +0900

    제곱근을 사용하여 기능개선 하지만 그리드 알고리즘을 사용하므로 모든 경우에 대하여 최적화된 결과를 나타내지 못함.

--- src/datastructure/queues/down_to_zero_ii/Solution.java
+++ src/datastructure/queues/down_to_zero_ii/Solution.java
@@ -34,50 +34,31 @@ public class Solution {
         System.out.println(cnt);
         return;
       }
-      if(isPrime(n)){
+/*      if(isPrime(n)){
+        n--;
+        continue;
+      }*/
+      int a = (int)Math.sqrt(n);
+      int b = 1;
+      while (a>0){
+        if(n%a==0){
+          b = n/a;
+          break;
+        }
+        --a;
+      }
+
+      if(a==1){
         n--;
         continue;
       }
 
-      Queue<Integer> queue = new LinkedList<>();
-
-      int prime = 2;
-      while(true){
-        if(n%prime==0){
-          queue.add(prime);
-          int i = n/prime;
-          if(isPrime(i)){
-            queue.add(i);
-            break;
-          }
-          else{
-            n=i;
-            prime = 2;
-          }
-        }
-        else {
-          prime = getNextPrime(prime);
-        }
-      }
-
-      int half = queue.size()/2;
-      int a = 1;
-      int b = 1;
-
-      for(int i=0; !queue.isEmpty(); i++){
-        // queue에서 절반으로 나눠서 각자 곱한값을 a 와 b 로 가정(하지만 틀렸음)
-        if(i<half){
-          a *= queue.poll();
-        }
-        else {
-          b *= queue.poll();
-        }
-      }
-      //System.out.println(a + " x " + b);
+      //System.out.println(n + " = " + a + " x " + b);
 
       n = Math.max(a, b);
     }
   }
+
   public static void main(String[] args){
     Scanner in = new Scanner(System.in);
     int Q = in.nextInt();
